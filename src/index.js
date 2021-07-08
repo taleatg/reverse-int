34397f6a83bf9aaa2b45be96f6a20e36029ae22b
Tatsiana Dashuk <t_korbyt@mail.ru>
2021-07-08 22:10:56 +0300

    feat: start task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+  let reversed = String(n).split('');
+  if (reversed[0] === '-') {
+      reversed.reverse().pop();
+  } else if (reversed[reversed.length - 1] === 0) {
+    reversed.reverse().shift();
+  } else {
+    reversed.reverse();
+  }
+  return reversed.join('');
 }
